dd03b541a803057d8b0dc82020cceb5a3c238516
Evan Thomas <thomasevan1248@gmail.com>
2025-09-06 20:33:34 -0400

    Created Task Data Class

---
+++ ToDo/Task.swift
@@ -0,0 +1,22 @@
+//
+//  Task.swift
+//  ToDo
+//
+//  Created by Student_User on 9/6/25.
+//
+
+import SwiftData
+import Foundation
+
+@Model
+class Task {
+    var timeStamp : Date
+    var title : String
+    var desc : String
+    
+    init(timeStamp: Date, title: String, desc: String){
+        self.timeStamp = timeStamp
+        self.title = title
+        self.desc = desc
+    }
+}
